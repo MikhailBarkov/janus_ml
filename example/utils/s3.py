f6b0e6ecd7d18a0d46a43783f755b7ab23aad4a3
MikhailBarkov <mikhail.barkov.2002@mail.ru>
2023-10-10 13:42:30 +0300

    fix bug in create buckets util

--- example/utils/s3.py
+++ example/utils/s3.py
@@ -1,9 +1,10 @@
 import boto3
 
 
+session = boto3.session.Session()
 s3 = session.client(
     service_name='s3',
-    endpoint_url=s3_endpoint,
+    endpoint_url="http://localhost:9000",
     aws_access_key_id='123',
     aws_secret_access_key='12345678'
 )
